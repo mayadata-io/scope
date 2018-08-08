7464c4cc10b3ac4c297df21160b3ef1e6ec2f5bd
Akash Srivastava <akashsrivastava4927@gmail.com>
2018-08-08 18:57:33 +0530

    Update client-go version to v8.0.0 (#96)
    * Update client-go version to v8.0.0
    
    - Update k8s.io/api version to kubernetes-1.11.0
    - Update k8s.io/apimachinery version to kubernetes-1.11.0
    - Update github.com/json-iterator/go version
    - Update NDM clientset
    
    Signed-off-by: Akash Srivastava <akash.srivastava@openebs.io>
    
    * Add openebs/maya dependency into Scope
    
    Signed-off-by: Akash Srivastava <akash.srivastava@openebs.io>

--- vendor/github.com/openebs/node-disk-manager/pkg/client/clientset/versioned/typed/openebs.io/v1alpha1/fake/fake_disk.go
+++ vendor/github.com/openebs/node-disk-manager/pkg/client/clientset/versioned/typed/openebs.io/v1alpha1/fake/fake_disk.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2018 The OpenEBS Authors
+Copyright The Kubernetes Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -59,7 +59,7 @@ func (c *FakeDisks) List(opts v1.ListOptions) (result *v1alpha1.DiskList, err er
 	if label == nil {
 		label = labels.Everything()
 	}
-	list := &v1alpha1.DiskList{}
+	list := &v1alpha1.DiskList{ListMeta: obj.(*v1alpha1.DiskList).ListMeta}
 	for _, item := range obj.(*v1alpha1.DiskList).Items {
 		if label.Matches(labels.Set(item.Labels)) {
 			list.Items = append(list.Items, item)
