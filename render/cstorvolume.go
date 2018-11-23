1a5817f71c096f4c5f9f7fb5fbf0bd8a0280ff97
Satyam Zode <satyamzode@gmail.com>
2018-11-23 15:48:46 +0530

    Add cStor support in Scope
    * Add support for CV, CVR & CSP in Scope
    
    * Add proper shapes for CV, CVR & CSP
    
    * Add status field to CV, CVR & CSP
    
    * Remove unnecessary filter in the APITopologyGroup
    
    * Remove unsed PodComponent method
    
    * Filter disk when Show Pods in Volumes
    
    Signed-off-by: Satyam Zode <satyamzode@gmail.com>

---
+++ render/cstorvolume.go
@@ -0,0 +1,48 @@
+package render
+
+import (
+	"context"
+
+	"github.com/weaveworks/scope/probe/kubernetes"
+	"github.com/weaveworks/scope/report"
+)
+
+//CStorVolumeRenderer is a Renderer which produces a renderable openebs CV.
+var CStorVolumeRenderer = cStorVolumeRenderer{}
+
+//cStorVolumeRenderer is a Renderer to render CStor Volumes.
+type cStorVolumeRenderer struct{}
+
+//Render renders the CV.
+func (v cStorVolumeRenderer) Render(ctx context.Context, rpt report.Report) Nodes {
+	cStorNodes := make(report.Nodes)
+
+	for cvID, cvNode := range rpt.CStorVolume.Nodes {
+		cStorNodes[cvID] = cvNode
+	}
+
+	for cspID, cspNode := range rpt.CStorPool.Nodes {
+		cStorNodes[cspID] = cspNode
+	}
+
+	for cvrID, cvrNode := range rpt.CStorVolumeReplica.Nodes {
+		cStorVolume, _ := cvrNode.Latest.Lookup(kubernetes.CStorVolumeName)
+		cStorVolumeNodeID := report.MakeCStorVolumeNodeID(cStorVolume)
+
+		if cvNode, ok := cStorNodes[cStorVolumeNodeID]; ok {
+			cvNode.Adjacency = cvNode.Adjacency.Add(cvrID)
+			cvNode.Children = cvNode.Children.Add(cvrNode)
+			cStorNodes[cStorVolumeNodeID] = cvNode
+		}
+
+		cStorPoolUID, _ := cvrNode.Latest.Lookup(kubernetes.CStorPoolUID)
+		cStorPoolNodeID := report.MakeCStorPoolNodeID(cStorPoolUID)
+		if cStorPoolNode, ok := cStorNodes[cStorPoolNodeID]; ok {
+			cvrNode.Children = cvrNode.Children.Add(cStorPoolNode)
+		}
+
+		cStorNodes[cvrID] = cvrNode
+	}
+
+	return Nodes{Nodes: cStorNodes}
+}
