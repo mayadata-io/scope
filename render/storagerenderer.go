b60a843c4ae090947adc7677b43624a86dd73a6d
Satyam Zode <satyamzode@gmail.com>
2018-11-26 19:20:37 +0530

    Add adjacency for the CSP, Disk & SPC
    * Add adjacency for the CSP, Disk & SPC
    
    * Fix comments for renderer
    
    Signed-off-by: Satyam Zode <satyamzode@gmail.com>

--- render/storagerenderer.go
+++ render/storagerenderer.go
@@ -2,44 +2,77 @@ package render
 
 import (
 	"context"
+	"strings"
 
+	"github.com/weaveworks/scope/probe/kubernetes"
 	"github.com/weaveworks/scope/report"
 )
 
 // KubernetesStorageRenderer is a Renderer which combines all Kubernetes
-// storage components such as storage pools, storage pool claims and disks.
+// storage components such as CstorPools, storage pool claims and disks.
 var KubernetesStorageRenderer = MakeReduce(
-	SPCToSPRenderer,
-	SPToDiskRenderer,
+	SPCToCSPRenderer,
+	CSPToDiskRenderer,
 )
 
-// SPCToSPRenderer is a Renderer which produces a renderable kubernetes CRD SPC
-var SPCToSPRenderer = spcToSpRenderer{}
+// SPCToCSPRenderer is a Renderer which produces a renderable kubernetes CRD SPC
+var SPCToCSPRenderer = spcToCSPRenderer{}
 
-// spcToSpRenderer is a Renderer to render SPC & SP nodes.
-type spcToSpRenderer struct{}
+// spcToCSPRenderer is a Renderer to render SPC & CSP nodes.
+type spcToCSPRenderer struct{}
 
-// Render renders the SPC & SP nodes with adjacency.
-// Here we are obtaining the spc name from sp and adjacency is created by matching it with spc name.
-func (v spcToSpRenderer) Render(ctx context.Context, rpt report.Report) Nodes {
+// Render renders the SPC & CSP nodes with adjacency.
+// Here we are obtaining the spc name from csp and adjacency is created by matching it with spc name.
+func (v spcToCSPRenderer) Render(ctx context.Context, rpt report.Report) Nodes {
 	nodes := make(report.Nodes)
 	for spcID, spcNode := range rpt.StoragePoolClaim.Nodes {
-		nodes[spcID] = spcNode
+		spcName, _ := spcNode.Latest.Lookup(kubernetes.Name)
+		for cspID, cspNode := range rpt.CStorPool.Nodes {
+			storagePoolCaimName, _ := cspNode.Latest.Lookup(kubernetes.StoragePoolClaimName)
+			if storagePoolCaimName == spcName {
+				spcNode.Adjacency = spcNode.Adjacency.Add(cspID)
+				spcNode.Children = spcNode.Children.Add(cspNode)
+			}
+			nodes[spcID] = spcNode
+		}
+
 	}
 	return Nodes{Nodes: nodes}
 }
 
-// SPToDiskRenderer is a Renderer which produces a renderable kubernetes CRD Disk
-var SPToDiskRenderer = spToDiskRenderer{}
+// CSPToDiskRenderer is a Renderer which produces a renderable kubernetes CRD Disk
+var CSPToDiskRenderer = cspToDiskRenderer{}
 
-// spToDiskRenderer is a Renderer to render SP & Disk .
-type spToDiskRenderer struct{}
+// cspToDiskRenderer is a Renderer to render CSP & Disk .
+type cspToDiskRenderer struct{}
 
-// Render renders the SP & Disk nodes with adjacency.
-func (v spToDiskRenderer) Render(ctx context.Context, rpt report.Report) Nodes {
+// Render renders the CSP & Disk nodes with adjacency.
+func (v cspToDiskRenderer) Render(ctx context.Context, rpt report.Report) Nodes {
+	// var disks []string
+
 	nodes := make(report.Nodes)
-	for diskID, diskNode := range rpt.Disk.Nodes {
-		nodes[diskID] = diskNode
+	for cspID, cspNode := range rpt.CStorPool.Nodes {
+		cspDiskPaths, _ := cspNode.Latest.Lookup(kubernetes.DiskList)
+		cspHostname, _ := cspNode.Latest.Lookup(kubernetes.HostName)
+
+		diskList := strings.Split(cspDiskPaths, "~p$")
+
+		for diskID, diskNode := range rpt.Disk.Nodes {
+			diskHostname, _ := diskNode.Latest.Lookup(kubernetes.HostName)
+			diskPaths, _ := diskNode.Latest.Lookup(kubernetes.DiskList)
+			diskPathList := strings.Split(diskPaths, "~p$")
+			for _, cspDiskPath := range diskList {
+				for _, diskPath := range diskPathList {
+					if (cspDiskPath == diskPath) && (cspHostname == diskHostname) {
+						cspNode.Adjacency = cspNode.Adjacency.Add(diskID)
+						cspNode.Children = cspNode.Children.Add(diskNode)
+					}
+				}
+			}
+			nodes[diskID] = diskNode
+		}
+		nodes[cspID] = cspNode
 	}
+
 	return Nodes{Nodes: nodes}
 }
